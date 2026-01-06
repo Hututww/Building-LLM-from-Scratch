c766631b873f2822f2516fdc1491acb23002f97d
饭斗花园土土大王 <stevechen2005@outlook.com>
2026-01-06 22:34:29 +0800

    第二部分完成😸接下来该喂数据集看看效果了！

--- Assignment_1/main/cs336_basics/BPE_trainer.py
+++ Assignment_1/main/cs336_basics/BPE_trainer.py
@@ -2,8 +2,7 @@ import regex as re
 from typing import Iterable
 from BPE_tokenizer import Tokenizer, PAT
 
-def train_bpe(self, 
-              input_path: str, 
+def train_bpe(input_path: str, 
               vocab_size: int, 
               special_tokens: list[str]
               ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
@@ -17,7 +16,6 @@ def train_bpe(self,
         vocab: 分词器词汇表(token ID → token字节)
         merges: BPE合并操作序列(按合并顺序排列)
     """
-    special_bytes = [special_tokens.encode("utf-8") for elem in special_tokens] 
     tokenizer = Tokenizer(vocab = {}, merges = [], special_tokens = special_tokens)
 
     vocab = {}
@@ -28,7 +26,7 @@ def train_bpe(self,
         vocab[initial_place + byte] = bytes([byte]) # 内层括号是防止生成依托空字节的
     
     initial_vocab_size = len(vocab)
-    if initial_vocab_size <= vocab_size:
+    if initial_vocab_size >= vocab_size:
         return vocab, []
     
     max_merge_times = vocab_size - initial_vocab_size
@@ -41,8 +39,9 @@ def train_bpe(self,
         with open(input_path, "r", encoding = "utf-8") as f:
             for line in f:
                 line = line.strip()
-                if line is None:
-                    yield line
+                if not line :
+                    continue
+                yield line
     
     byte_seq = []
     for text in _file_loader():
@@ -53,7 +52,41 @@ def train_bpe(self,
             single_byte_seq = [bytes([i]) for i in elem]
             byte_seq.append(single_byte_seq)
 
-        if byte_seq is None:
-            return vocab, []
+    if not byte_seq : # 注意not和is None的区别！
+        return vocab, []
     
     # 合并时间
+    for elem in range(max_merge_times):
+        freq = {}
+        for seq in byte_seq:
+            if len(seq) < 2:
+                continue
+            for i in range(len(seq) - 1):
+                pair = (seq[i], seq[i+1])
+                if pair in freq:
+                    freq[pair] += 1
+                else:
+                    freq[pair] = 1
+        if not freq: break
+
+        best_pair = max(freq, key = freq.get)
+        merges.append(best_pair)
+
+        merged_byte = b"".join(best_pair)
+        vocab[len(vocab)] = merged_byte
+
+        new_seq = []
+        for seq in byte_seq:
+            _new_seq = []
+            i = 0
+            while i < len(seq):
+                if i < len(seq) - 1 and (seq[i], seq[i+1]) == best_pair:
+                    _new_seq.append(merged_byte)
+                    i += 2
+                else:
+                    _new_seq.append(seq[i])
+                    i += 1
+            new_seq.append(_new_seq)
+        byte_seq = new_seq
+
+    return vocab, merges
