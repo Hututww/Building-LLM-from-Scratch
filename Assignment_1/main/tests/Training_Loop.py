dd22e17702fc15c6622e16101e304d01df297fdc
饭斗花园土土大王 <stevechen2005@outlook.com>
2026-02-03 23:01:28 +0800

    完成了训练流程的实现！第五章搞定😆

--- Assignment_1/main/tests/Training_Loop.py
+++ Assignment_1/main/tests/Training_Loop.py
@@ -1,5 +1,8 @@
+import os
 import torch
 import numpy as np
+
+from .Training_Utils_for_Trans import calc_cross_entropy_loss, gradient_clipping
 
 def get_batch(x: np.ndarray, batch_size: int, context_length: int, device):
     max_begin_idx = len(x) - context_length - 1
@@ -25,3 +28,38 @@ def load_checkpoint(src, model, optimizer):
     optimizer.load_state_dict(checkpoint["optimizer_state"])
 
     return checkpoint["iteration"]
+
+def train(model, 
+          optimizer, 
+          train_data_path, 
+          max_iters=1000, 
+          batch_size=3, 
+          context_length=128,
+          device="cpu",
+          checkpoint_path="checkpoints"
+          ):
+    # 暂时还没加验证的逻辑
+    os.makedirs(checkpoint_path, exist_ok=True)
+    train_data = np.memmap(train_data_path, dtype=np.uint16, mode="r")
+
+    model.to(device)
+    model.train()
+
+    for i in range(max_iters):
+        inputs, targets = get_batch(train_data, batch_size, context_length, device)
+
+        logits = model(inputs)
+
+        loss = calc_cross_entropy_loss(logits, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        gradient_clipping(model.parameters(), l2_max=1.0)
+
+        optimizer.step()
+
+        if i % 10 == 0:
+            print(f"迭代 {i}/{max_iters}, 损失: {loss.item():.4f}")
+
+    print("训练搞定！")
