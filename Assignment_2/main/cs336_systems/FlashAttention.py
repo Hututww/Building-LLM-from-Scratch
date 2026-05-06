e19c0f27b12db54a1cec39bf238f67d83d630e3a
饭斗花园土土大王 <stevechen2005@outlook.com>
2026-05-06 18:15:45 +0800

    修正了一些错误！开始推进

--- Assignment_2/main/cs336_systems/FlashAttention.py
+++ Assignment_2/main/cs336_systems/FlashAttention.py
@@ -2,7 +2,52 @@ import torch
 import triton
 import triton.language as tl
 
-def forward(ctx, Q, K, V, is_causal=False):
+@triton.jit
+def _flash_fwd_kernel(Q, K, V, softmax_scale, l, output,
+                      stride_qb, stride_qh, stride_qm, stride_qk,
+                      stride_kb, stride_kh, stride_kn, stride_kk,
+                      stride_vb, stride_vh, stride_vn, stride_vk,
+                      stride_ob, stride_oh, stride_om, stride_ok,
+                      batch_size, num_heads, seq_len, head_dim,
+                      BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr,
+):
+    """
+    stride_qb	Stride of Q's Batch	    要从第一个 Batch 跳到第二个 Batch，需要跨过多少个数字。
+    stride_qh	Stride of Q's Head	    在同一个 Batch 里，从第一个“头”跳到第二个“头”需要走多少步。
+    stride_qm	Stride of Q's M-axis	在同一个“头”里，从第一行 Token 跳到第二行 Token 需要走多少步。
+    stride_qk	Stride of Q's K-dim	    在同一行里，从第一个特征值跳到第二个特征值需要走多少步。
+    """
+    program_id = tl.programid(0)
+
+    offset_headbatch = tl.programid(1)
+    off_b = offset_headbatch // num_heads
+    off_h = offset_headbatch % num_heads
+
+    q_ptr = Q + off_b * stride_qb + off_h * stride_qh
+    k_ptr = K + off_b * stride_kb + off_h * stride_kh
+    v_ptr = V + off_b * stride_vb + off_h * stride_vh
+    o_ptr = output + off_b * stride_ob + off_h * stride_oh
+    l_ptr = l + offset_headbatch * seq_len
+
+    row_m = tl.arange(0, BLOCK_M) + program_id * BLOCK_M
+    row_n = tl.arange(0, BLOCK_N)
+    row_k = tl.arange(0, head_dim)
+
+    q = tl.load(q_ptr + row_m[:, None] * stride_qm + row_k[None, :] * stride_qk)
+
+    a_max = tl.full([BLOCK_M], -float('inf'), dtype=tl.float32)
+    exp_sum = tl.zeros([BLOCK_M], dtype=tl.float32)
+    total = tl.zeros([BLOCK_M, head_dim], dtype=tl.float32)
+    
+
+class flash_forward_pass_pytorch(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, Q, K, V, is_causal=False):
+        """
+        纯PyTorch实现FlashAttention-2前向（作业1.3.2a要求）
+        输入形状：[batch_size, num_heads, seq_len, head_dim]
+        输出：output, l (logsumexp)
+        """
         device = Q.device
         dtype = Q.dtype
         batch_size, num_heads, seq_len, head_dim = Q.shape
@@ -42,46 +87,8 @@ def forward(ctx, Q, K, V, is_causal=False):
 
         ctx.save_for_backward(Q, K, V, output, log_sum_exp)
         ctx.is_causal = is_causal
-        return output
-
-
-# ===================================== triton实现 =====================================
-@triton.jit
-def _flash_fwd_kernel(Q, K, V, softmax_scale, l, output,
-                      stride_qb, stride_qh, stride_qm, stride_qk,
-                      stride_kb, stride_kh, stride_kn, stride_kk,
-                      stride_vb, stride_vh, stride_vn, stride_vk,
-                      stride_ob, stride_oh, stride_om, stride_ok,
-                      batch_size, num_heads, seq_len, head_dim,
-                      BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr,
-):
-    """
-    stride_qb	Stride of Q's Batch	    要从第一个 Batch 跳到第二个 Batch，需要跨过多少个数字。
-    stride_qh	Stride of Q's Head	    在同一个 Batch 里，从第一个“头”跳到第二个“头”需要走多少步。
-    stride_qm	Stride of Q's M-axis	在同一个“头”里，从第一行 Token 跳到第二行 Token 需要走多少步。
-    stride_qk	Stride of Q's K-dim	    在同一行里，从第一个特征值跳到第二个特征值需要走多少步。
-    """
-    program_id = tl.programid(0)
-
-    offset_headbatch = tl.programid(1)
-    off_b = offset_headbatch // num_heads
-    off_h = offset_headbatch % num_heads
-
-    q_ptr = Q + off_b * stride_qb + off_h * stride_qh
-    k_ptr = K + off_b * stride_kb + off_h * stride_kh
-    v_ptr = V + off_b * stride_vb + off_h * stride_vh
-    o_ptr = output + off_b * stride_ob + off_h * stride_oh
-    l_ptr = l + offset_headbatch * seq_len
-
-    row_m = tl.arange(0, BLOCK_M) + program_id * BLOCK_M
-    row_n = tl.arange(0, BLOCK_N)
-    row_k = tl.arange(0, head_dim)
-
-    q = tl.load(q_ptr + row_m[:, None] * stride_qm + row_k[None, :] * stride_qk)
-
-    a_max = tl.full([BLOCK_M], -float('inf'), dtype=tl.float32)
-    exp_sum = tl.zeros([BLOCK_M], dtype=tl.float32)
-    total = tl.zeros([BLOCK_M, head_dim], dtype=tl.float32)
+        return output, log_sum_exp
     
-class flash_forward_pass_pytorch(torch.autograd.Function):
-     def forward():
+    @staticmethod
+    def backward(ctx, output, log_sum_exp):
+        raise NotImplementedError("not implemented")
